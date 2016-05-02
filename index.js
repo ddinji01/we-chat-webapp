87e519065ff5d851fe06a50c3954af7f18cbdd8a
Daniel Dinjian <daniel.dinjian@tufts.edu>
2016-05-02 18:09:26 -0400

    Notifications on user join or leave chatroom, site scrolls to bottom to stay with new message.  Users have usernames, messages are displayed with username

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ app.set('port', (process.env.PORT || 3000));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
-
 io.on('connection', function(socket) {
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
+	var socket_user = null;
+    socket.on('chat message', function(msg, user) {
+        io.emit('chat message', msg, user);
+    });
+    socket.on('join', function(user) {
+    	socket_user = user;
+    	io.emit('gained-user', user);
+    });
+    socket.on('disconnect', function() {
+    	if (socket_user != null) {
+    		io.emit('lost-user', socket_user)
+    	}
     });
 });
-
 http.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
